ff7803e0a1bb47c8aaeb4f2a450d39247e53b9c8
shivanisinha24 <sinhashivani010@gmail.com>
2026-08-12 20:34:45 +0530

    updated one

--- 2D Array/Rowofmaxsum.java
+++ 2D Array/Rowofmaxsum.java
@@ -2,13 +2,17 @@ public class Rowofmaxsum{
   public static void main(String[] args){
     int[][] arr ={{1,2,3,4},{5,67,88,91},{32,55,4,87}};
     int maxSum =Integer.MIN_VALUE;
+    int row = -1;
     for(int i=0; i<arr.length;i++){
       int sum =0;
       for(int j =0;j<arr[0].length;j++){
         sum +=arr[i][j];
       }
-      maxSum = Math.max(maxSum,sum);
+      if(sum>maxSum){
+        maxSum = sum;
+        row =i;
+      }
     }
-    System.out.print(maxSum);
+    System.out.print(row+" "+maxSum);
   }
 }
