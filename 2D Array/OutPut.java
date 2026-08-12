b21f7360bfbda420a5fcc04eb37dd6a0146fb391
shivanisinha24 <sinhashivani010@gmail.com>
2026-08-12 19:54:24 +0530

    new topic

---
+++ 2D Array/OutPut.java
@@ -0,0 +1,12 @@
+public class InputoutPut {
+    public static void main(String[] args) {
+        int[][] arr = {{1,2,3,5},{4,5,6,77},{7,8,9,88}};
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + "   ");
+            }
+            System.out.println();
+        }
+    }
+}
