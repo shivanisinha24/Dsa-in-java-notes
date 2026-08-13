60dbb1ebfa890af02f3863f53736b64ee4286c12
shivanisinha24 <sinhashivani010@gmail.com>
2026-08-13 10:22:46 +0530

    today learning

---
+++ 2D Array/columwiseprint.java
@@ -0,0 +1,11 @@
+public class columwiseprint{
+  public static void main(String[] args){
+    int[][] arr ={{6,0,2,7,6},{1,3,7,20,2},{9,9,4,5,2}};
+    for(int j =0;j<arr[0].length;j++){
+      for(int i=0;i<arr.length;i++){
+        System.out.print(arr[i][j]+ " ");
+      }
+    System.out.println();
+    }
+  }
+}
---
+++ 2D Array/foreachloop.java
@@ -0,0 +1,11 @@
+public class foreachloop{
+  public static void main(String[] args){
+    int[][] arr = {{6,0,2,7,6},{1,3,7,20,2},{9,9,4,5,2}};
+    for(int i=0;i<arr.length;i++){
+      for(int ele : arr[i]){
+        System.out.print(ele+" ");
+      }
+      System.out.println();
+    }
+  }
+}
